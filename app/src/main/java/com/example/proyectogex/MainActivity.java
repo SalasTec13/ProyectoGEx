e72943d342a38b4e6ab51ee29b798c388dff4535
Melissa Vianey Villegas Cortés <43429295+MVianeyVC@users.noreply.github.com>
2022-10-22 00:33:32 -0700

    Calendario y pantallas anteriores

--- app/src/main/java/com/example/proyectogex/MainActivity.java
+++ app/src/main/java/com/example/proyectogex/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity{
     /*variable para cambiar de ventana(Main a Information extinguisher)*/
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity{
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, InformationExtinguisherActivity.class);
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(i);
             }
         });
